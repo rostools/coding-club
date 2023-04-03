ed522cc586ef8448bbc48e0c4376f25ba92e440b
Stefania Noerman <stefania.yang@gmail.com>
2023-04-03 15:02:12 +0200

    generate the website

---
+++ posts/making-websites/230403_making_website.R
@@ -0,0 +1,12 @@
+#set original repo as the upstream
+gert::git_remote_add ("https://github.com/rostools/coding-club.git", "upstream")
+
+#create a branch
+usethis::pr_init("add-action")
+
+#see a list of branches
+gert::git_branch_list()
+
+#create a github folder
+usethis::use_github_action("build-website",
+                           url = "https://raw.githubusercontent.com/quarto-dev/quarto-actions/main/examples/quarto-publish-example.yml")
